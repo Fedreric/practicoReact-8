6c7646ffc8324bb8824120718b7a5283ad20bd72
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-23 16:37:30 -0300

    Se agrega los states a cada input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,19 +1,25 @@
 import { Form, Button, Container } from "react-bootstrap";
+import { useState } from "react";
 const Formulario = () => {
-  return (
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [dni, setDni] = useState('');
+    const [email, setEmail] = useState('');
+
+    return (
     <Container>
       <Form>
         <Form.Group className="mb-3" controlId="nombre">
-          <Form.Control type="text" placeholder="Nombre EJ: Federico" />
+          <Form.Control type="text" placeholder="Nombre EJ: Federico" onChange={(e)=>setNombre(e.target.value)} value={nombre}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="apellido">
-          <Form.Control type="text" placeholder="Apellido Ej: Ledesma" />
+          <Form.Control type="text" placeholder="Apellido Ej: Ledesma" onChange={(e)=>setApellido(e.target.value)} value={apellido}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="dni">
-          <Form.Control type="number" placeholder="D.N.I Ej: 44555777" />
+          <Form.Control type="number" placeholder="D.N.I Ej: 44555777" onChange={(e)=>setDni(e.target.value)} value={dni}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="email">
-          <Form.Control type="email" placeholder="Email Ej: fede@gmail.com" />
+          <Form.Control type="email" placeholder="Email Ej: fede@gmail.com" onChange={(e)=>setEmail(e.target.value)} value={email}/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Enviar
