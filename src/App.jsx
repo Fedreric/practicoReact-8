fba82bdef4dfd4a0564b7df261137f8132fa7baa
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-23 16:11:31 -0300

    Se agrega un banner principal que contiene un h1("Formulario")

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <section>
+      <Container className='bg-dark'>
+      <h1 className='text-center mt-1 text-light py-3'>Formulario</h1>
+      </Container>
+    </section>
   )
 }
 
