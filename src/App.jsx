35950540788a6b8a17678bcda7c52b0d450d1ca3
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-23 17:12:57 -0300

    Se agrega las clases en los contenedores necesarios para el correcto funcionamiento del footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap';
 import './App.css'
 import Formulario from './components/Formulario';
+import FooterFedeLedesma from './components/FooterFedeLedesma';
 
 function App() {
   return (
-    <section>
+    <>
+    <section className='mainPage'>
       <Container className='bg-dark'>
       <h1 className='text-center mt-1 text-light py-3'>Formulario</h1>
       </Container>
       <Formulario></Formulario>
     </section>
+      <FooterFedeLedesma></FooterFedeLedesma>
+    </>
   )
 }
 
