2bf95460310afe0b24f15090c08c3dc1e726b432
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-23 16:16:59 -0300

    Se hace uso del componente Fomulario.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap';
 import './App.css'
+import Formulario from './components/Formulario';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Container className='bg-dark'>
       <h1 className='text-center mt-1 text-light py-3'>Formulario</h1>
       </Container>
+      <Formulario></Formulario>
     </section>
   )
 }
